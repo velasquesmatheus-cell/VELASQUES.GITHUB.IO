5b7e406eb82f850a6c91109d5c0cc5c0c56508f2
velasquesmatheus-cell <velasques.matheus@escola.pr.gov.br>
2026-03-19 10:50:32 -0300

    Update cocuda.js

--- cocuda.js
+++ cocuda.js
@@ -1,1 +1,20 @@
+let contador = 0;
 
+function atualizarTela() {
+    document.getElementById("contador").innerText = contador;
+}
+
+function aumentar() {
+    contador++;
+    atualizarTela();
+}
+
+function diminuir() {
+    contador--;
+    atualizarTela();
+}
+
+function resetar() {
+    contador = 0;
+    atualizarTela();
+}
